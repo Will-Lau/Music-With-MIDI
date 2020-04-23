5bf4bce420403e74f4a989e2d9418257d19524b1
Will-Lau <1768441047@qq.com>
2020-04-23 14:57:25 +0800

    txt to mid and mid to txt

---
+++ ConsoleApp1/BinToChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+    class BinToChar
+    {
+        static void Main(string[] args)
+        {
+            //读
+            BinaryReader binaryReader;
+            FileStream fs = new FileStream("C:\\Users\\ann_l\\Downloads\\hgrywolf.mid", FileMode.Open);
+            binaryReader = new BinaryReader(fs);
+            long length = fs.Length;
+            byte[] bytes;
+            bytes = binaryReader.ReadBytes((int)length);
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0:x2} ", bytes[i]);
+            }
+
+            //写
+            if (!File.Exists("C:\\Users\\ann_l\\Downloads\\out.txt"))
+            {
+                File.Create("C:\\Users\\ann_l\\Downloads\\out.txt");
+            }
+
+            // FileStream output = new FileStream("C:\\Users\\ann_l\\Downloads\\out.txt", FileMode.Open);
+            StreamWriter streamWriter = new StreamWriter("C:\\Users\\ann_l\\Downloads\\out.txt", true);
+            for (int i = 0; i < length; i++)
+            {
+                streamWriter.Write("{0:x2} ", bytes[i]);
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/CharToBin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class CharToBin
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("C:\\Users\\ann_l\\Downloads\\souce.txt");
+            String input = streamReader.ReadToEnd();
+            input = input.Replace("\n", "");
+            input = input.Replace("\r", " ");
+            string[] inputs = input.Split(" ");
+            int len = inputs.Length;
+            byte[] bytes = new byte[len];
+            for (int i = 0; i < len - 1; i++)
+            {
+                bytes[i] = Convert.ToByte(inputs[i], 16);
+            }
+
+            FileStream fs = new FileStream("C:\\Users\\ann_l\\Downloads\\output.mid", FileMode.OpenOrCreate);
+            BinaryWriter binaryWriter = new BinaryWriter(fs);
+            fs.Write(bytes, 0, len);
+            fs.Close();
+
+        }
+    }
+}
